d4750d389c272edb88b06b54b7b4a7dd255d9bf3
Johnson Hsu <specialline4107@gmail.com>
2024-11-21 22:29:15 +0800

    Update second version

--- password_guess.py
+++ password_guess.py
@@ -1,18 +1,18 @@
 # Guess password
 
 password = 'a123456'
-guess_num = 0
-
+guess_num = 3
+ 
 while True:
     guess = input('Please input password: ')
     if guess == password:
         print('Sign in')
         break
     else:
+        guess_num -= 1
         print('Password is wrong')
-        print('You still have', 2 - guess_num, 'chances' )
-        guess_num += 1
-        if guess_num == 3:
+        print('You still have', guess_num, 'chances' )
+        if guess_num == 0:
             print('Fail to sign in')
             break
 
